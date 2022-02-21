f537fb71c23d9570d64d5fd12027c398c1af2d4e
Titouan <titouan.plomb@outlook.com>
2022-02-21 16:09:08 +0100

    Ajout de la requete POST et gestion des codes HTTP (404, 503, 200, etc...)

--- client/capteur.py
+++ client/capteur.py
@@ -1,19 +1,36 @@
 import datetime
 import json
+import requests
+import time
 
-date = datetime.datetime.today().strftime('%Y-%m-%d %H')
-nom = date + '.json'
+date = datetime.datetime.today().strftime('%Y-%m-%d %H%M%S')
+nom = "C:/Users/titou/Projet/ProjetMeteo/json/" + date + '.json'
 
-Temperature = int(12)
-Pression = int(1023)
-Hygrometrie = int(50)
+def loop():
+    
+    date = datetime.datetime.today().strftime('%Y-%m-%d %H%M%S')
+    nom = "C:/Users/titou/Projet/ProjetMeteo/json/" + date + '.json'
+    
+    Temperature = int(12)
+    Pression = int(1023)
+    Hygrometrie = int(50)
+    
+    data = {
+        "Date": date,
+        "Temperature": Temperature,
+        "Pression": Pression,
+        "Hygrometrie": Hygrometrie
+        }
+        
+    DumpedData = json.dumps(data)
+    
+    req = requests.post('http://x.x.x.x/post', DumpedData)
+    
+    if req.status_code != 200:
+        with open(nom, 'w') as file:
+            json.dump(data, file)
 
-data = {
-    "Date": date,
-    "Temperature": Temperature,
-    "Pression": Pression,
-    "Hygrometrie": Hygrometrie
-}
+    time.sleep(3600)
+    loop()
 
-with open(nom, 'w') as file:
-    json.dump(data, file)
+loop()
