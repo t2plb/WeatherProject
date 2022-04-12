465f71f3bff683d9b673912a860bf19f616ab31d
Titouan <titouan.plomb@outlook.com>
2022-04-12 20:26:07 +0200

    BUG : Modification requete post

--- server/post.php
+++ server/post.php
@@ -7,7 +7,7 @@ if (isset($_GET['json'])) {
     $date = new DateTime($donnees['datetime']);
     InsertDonnee($date->format('Y-m-d H:i:s'), $donnees['temp'], $donnees['humi']);
 } else {
-    if (isset($_GET['date']) && isset($_GET['temp']) && isset($_GET['humi'])) {
+    if (isset($_GET['datetime']) && isset($_GET['temp']) && isset($_GET['humi'])) {
         $date = new DateTime($_GET['datetime']);
         InsertDonnee($date->format('Y-m-d H:i:s'), $_GET['temp'], $_GET['humi']);
     } else {
