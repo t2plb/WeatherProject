e6c4d03103a7e7beb1a16b74c1dfc04252b7d603
Titouan <titouan.plomb@outlook.com>
2022-05-01 00:41:36 +0200

    Ajouts des script + framework en local Ajout du datetimepicker Creation URL requete SELECT pour récuperer les enregistrement SQL Nettoyage de index.html

--- server/database.php
+++ server/database.php
@@ -4,7 +4,7 @@ function openDatabase(){
 
     global $bdd;
 
-    $bdd = new PDO('mysql:host=192.168.1.183;dbname=meteo;charset=utf8', 'createur', 'creat0r0taerc');
+    $bdd = new PDO('mysql:host=192.168.1.183;dbname=meteo;charset=utf8', 'username', 'passwd');
     $bdd->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 }
 
@@ -18,4 +18,18 @@ function InsertDonnee($date, $temperature, $humidite){
     $sql = "INSERT INTO donnees (date, temp, humi) VALUES (?,?,?)";
     $stmt= $bdd->prepare($sql);
     $stmt->execute([$date, $temperature, $humidite]);
+}
+
+function LireDonnee($from, $to){
+
+    global $bdd;
+
+    if($bdd == null){
+        openDatabase();
+    }
+    $sql = "SELECT date, temp, humi, FROM donnees WHERE (date >= ? AND date <= ?)";
+    $stmt = $bdd->prepare($sql);
+    $stmt->execute([$from, $to]);
+
+    return $stmt;
 }
