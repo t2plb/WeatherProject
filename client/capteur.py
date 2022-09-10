eb949a4bbff9606b9ad397563d27cd10f1d5ca85
t2plb <titouan.plomb@outlook.com>
2022-09-10 14:42:26 +0200

    Ajout du client en tant que service + modification capteur.py

--- client/capteur.py
+++ client/capteur.py
@@ -10,30 +10,23 @@ url = 'http://127.0.0.1/post.php'
 if not os.path.exists('./json'):
     os.makedirs('./json')
 
-def loop():
 
-    cap = Adafruit_DHT.DHT11
-    pin = 23
-    date = datetime.datetime.today().strftime('%Y-%m-%d %H%M%S')
-    nom = "./json/" + date + '.json'
+cap = Adafruit_DHT.DHT11
+pin = 23
+date = datetime.datetime.today().strftime('%Y-%m-%d %H%M%S')
+nom = "./json/" + date + '.json'
     
-    Temperature, Humidite = Adafruit_DHT.read_retry(cap, pin)
+Humidite, Temperature = Adafruit_DHT.read_retry(cap, pin)
     
-    data = {
-        "datetime": date,
-        "temp": Temperature,
-        "humi": Humidite
-        }
+data = {
+    "datetime": date,
+    "temp": Temperature,
+    "humi": Humidite
+    }
         
-    DumpedData = json.dumps(data)
+DumpedData = json.dumps(data)
     
-    req = requests.post(url, params={"json": DumpedData})
-    print(req.status_code)
-    if req.status_code != 200:
-        with open(nom, 'w') as file:
-            json.dump(data, file)
-
-    time.sleep(3600)
-    loop()
-
-loop()
+req = requests.post(url, params={"json": DumpedData})
+if req.status_code != 200:
+    with open(nom, 'w') as file:
+        json.dump(data, file)
